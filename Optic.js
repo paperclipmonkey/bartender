5cc173b17001a6040faa214adae88076c3ccb770
Mike Waterworth <mike.waterworth@epimorphics.com>
2020-12-27 12:49:03 +0000

    Support multiple openings from optic in one step

--- Optic.js
+++ Optic.js
@@ -2,25 +2,32 @@ const Pump = require('./Pump.js')
 const Servo = require('./Servos.js')
 const Logging = require('./Logging.js')
 
-const OPEN_TIME = 5000 // Hold open time waiting for optic to drain
-const OPENING_TIME = 2000
-const CLOSING_TIME = 3500
-const OPENING_TORQUE = 0.7 // 0-1. Needs to be strong enough to move the optic 
-const CLOSING_TORQUE = 0.4 // 0-1. Ensure we don't hit the stops
+const VOLUME = 25
+const REFILL_TIME = 2500 // Amount of time the optic takes to refill
+const OPEN_TIME = 2500 // Hold open time waiting for optic to drain
+const OPENING_TIME = 700 // Move from closed to open
+const CLOSING_TIME = 300 // Move from open to closed
+const OPENING_TORQUE = 0.8 // 0.5 - 1. Needs to be strong enough to move the optic 
+const CLOSING_TORQUE = 0.01 // 0-0.5. Faster closes encourage the bubbles to float up
 
 class Optic extends Pump {
     async dispense(amount) {
         Logging.log(`Dispensing ${amount}ml of ${this.name}`)
-        await this.open()
-        await this.delay(OPEN_TIME)
-        await this.close()
+        const numDispenses = Math.floor(amount / VOLUME)
+        console.log(numDispenses)
+        for (let i = 0; i < numDispenses; i++) {
+            await this.open()
+            await this.delay(OPEN_TIME)
+            await this.close()
+            await this.delay(REFILL_TIME)
+        }
     }
 
     // Estimate how long it'll take to dispense in ms
-    dispenseDuration() {
-        return (OPENING_TIME + OPEN_TIME + CLOSING_TIME)
+    dispenseDuration(amount) {
+        return (OPENING_TIME + OPEN_TIME + CLOSING_TIME + REFILL_TIME) * Math.floor(amount / VOLUME)
     }
-    
+
     // Open the Optic
     async open() {
         Logging.log(`Opening ${this.name}`)
