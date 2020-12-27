8f750d5b19e9c7dadb6b6d53c32169a238800c8c
Mike Waterworth <mike.waterworth@epimorphics.com>
2020-12-27 13:32:45 +0000

    Update optic with opening in the opposite direction

--- Optic.js
+++ Optic.js
@@ -2,25 +2,29 @@ const Pump = require('./Pump.js')
 const Servo = require('./Servos.js')
 const Logging = require('./Logging.js')
 
-const VOLUME = 25
-const REFILL_TIME = 2500 // Amount of time the optic takes to refill
-const OPEN_TIME = 2500 // Hold open time waiting for optic to drain
-const OPENING_TIME = 700 // Move from closed to open
-const CLOSING_TIME = 300 // Move from open to closed
-const OPENING_TORQUE = 0.8 // 0.5 - 1. Needs to be strong enough to move the optic 
-const CLOSING_TORQUE = 0.01 // 0-0.5. Faster closes encourage the bubbles to float up
+const VOLUME = 25 // ml of the optic
+const REFILL_TIME = 3000 // ms optic takes to refill
+const OPEN_TIME = 2800 // ms optic to drain
+const OPENING_TIME = 400 // ms move from closed to open
+const CLOSING_TIME = 275 // ms move from open to closed
+const OPENING_TORQUE = 0.0 // 0 - 0.5. Needs to be strong enough to move the optic 
+const CLOSING_TORQUE = 1 // 0.5 - 1. Faster closes encourage the bubbles to float up
 
 class Optic extends Pump {
     async dispense(amount) {
         Logging.log(`Dispensing ${amount}ml of ${this.name}`)
         const numDispenses = Math.floor(amount / VOLUME)
-        console.log(numDispenses)
+        // Open the optic x times
         for (let i = 0; i < numDispenses; i++) {
-            await this.open()
-            await this.delay(OPEN_TIME)
-            await this.close()
-            await this.delay(REFILL_TIME)
+            await this.dispenseCycle()
         }
+    }
+
+    async dispenseCycle() {
+        await this.open()
+        await this.delay(OPEN_TIME)
+        await this.close()
+        await this.delay(REFILL_TIME)
     }
 
     // Estimate how long it'll take to dispense in ms
@@ -46,6 +50,16 @@ class Optic extends Pump {
         await this.delay(CLOSING_TIME)
         Servo.setOff(this.pin)
         Logging.log(`Closed ${this.name}`)
+    }
+    
+    // Force air out of the optic?
+    async wiggle() {
+        await this.delay(1000)
+        Servo.setDutyCycle(this.pin, 0)
+        await this.delay(250)
+        Servo.setDutyCycle(this.pin, 1)
+        await this.delay(150)
+        Servo.setOff(this.pin)
     }
 }
 
